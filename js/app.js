743c1e935f295a1ca0885b259071991a9cf12b6f
kalana1985 <kalana1on1@gmail.com>
2017-07-19 00:19:17 -0500

    cddind css

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 //problem:user when clicking on image going to the dead end resulting poor user experiance 
 //solution:creating a overlay with the large image-light-box
+ var $overlay=$('<div id="overlay"></div>');
+ //jQuery allowes you to create a its repersantation even tho the element isnt part of the DOM yet.
+ //its common practies to use $ sign in jQuery oblects as $overLay variable.
+// Add overlay
+$("body").append($overlay);
+// An image
+   // A caption
 
-//1. Create the link event on a link to am image
+//1. Capture the link event on a link to am image
 $("#imageGallery a").click(function(event){
   event.preventDefault();
   $(this).attr("href");
-
+  $overlay.show();
   //2. Show the overlay
   //1.2 update overlay with the image linked in the link
   //1.3 get child's alt attri and set caption
 });
- //2 Add overlay
-   //2.1 An image
-   //2.2 A caption
+ 
 
- //3. When overlay is clicked
-   //3.1 Hide overlay   
+ // When overlay is clicked
+   // Hide overlay   
