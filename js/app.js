a1a077238137485ea7cb2cd04e1bce750d8bc3b1
kalana1985 <kalana1on1@gmail.com>
2017-07-18 21:39:32 -0500

    create a click event

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
-//problem:user when clicking on image going to the dead end
-//solution:creating a light-box
+//problem:user when clicking on image going to the dead end resulting poor user experiance 
+//solution:creating a overlay with the large image-light-box
+
+//1. Create the link event on a link to am image
+$("#imageGallery a").click(function(event){
+  event.preventDefault();
+  var x=$(this).attr("href");
+  console.log(x);
+
+  //2. Show the overlay
+  //1.2 update overlay with the image linked in the link
+  //1.3 get child's alt attri and set caption
+});
+ //2 Add overlay
+   //2.1 An image
+   //2.2 A caption
+
+ //3. When overlay is clicked
+   //3.1 Hide overlay   
