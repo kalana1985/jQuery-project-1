7bc75aa8642e31ab81997be18f01c2fc6c7f92f9
kalana1985 <kalana1on1@gmail.com>
2017-07-19 00:51:27 -0500

    adding changes to the app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 //problem:user when clicking on image going to the dead end resulting poor user experiance 
 //solution:creating a overlay with the large image-light-box
  var $overlay=$('<div id="overlay"></div>');
+ var $image=$("<img>");
+ $overlay.append($image);
  //jQuery allowes you to create a its repersantation even tho the element isnt part of the DOM yet.
  //its common practies to use $ sign in jQuery oblects as $overLay variable.
 // Add overlay
@@ -11,10 +13,13 @@ $("body").append($overlay);
 //1. Capture the link event on a link to am image
 $("#imageGallery a").click(function(event){
   event.preventDefault();
-  $(this).attr("href");
-  $overlay.show();
-  //2. Show the overlay
+  var imageLocation=$(this).attr("href");
+ 
+  
   //1.2 update overlay with the image linked in the link
+  $image.attr("src",imageLocation);
+  //2. Show the overlay
+  $overlay.show();
   //1.3 get child's alt attri and set caption
 });
  
